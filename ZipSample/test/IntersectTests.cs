f1bc5904829831231f897ed04067a2c625169bea
LeoSheu <leo850611@hotmail.com>
2018-12-01 15:47:23 +0800

    failed

--- ZipSample/test/IntersectTests.cs
+++ ZipSample/test/IntersectTests.cs
@@ -12,7 +12,7 @@ namespace ZipSample.test
         public void intersect_integers()
         {
             var first = new List<int> { 1, 3, 5 };
-            var second = new List<int> { 5, 3, 7, 9 };
+            var second = new List<int> { 5, 5, 3, 7, 9 };
 
             var expected = new List<int> { 3, 5 };
 
